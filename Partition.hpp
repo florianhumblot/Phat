f5e4468b878456efa1cad6a8075d6d9b0e4cec80
Florian Humblot <florian.humblot@student.hu.nl>
2018-06-26 14:55:37 +0200

    Added doxygen to the partition class

--- Partition.hpp
+++ Partition.hpp
@@ -22,18 +22,27 @@ private:
 	uint32_t TotalSectors;
 
 public:
+	/// \brief empty constructor for empty partition;
 	Partition(){}
+	
+	/// \brief Constructor for a partition table
+	/// \details Constructs a partition table based on the 16 byte entry found in the raw data of the SD card.
 	Partition( uint8_t PartitionTable[16] );
+
+	/// \brief Checks whether the partition is FAT32. 
 	bool isFAT32() {
 		return Sys_ID == SystemID::EXT_FAT32_LBA;
 	}
+
+	/// \brief Returns the fat table location
 	uint32_t getFatTableLocation() {
 		return RelativeSector;
 	}
+
+	/// \brief Returns the total amounf of sectors of the partition. 
 	uint32_t getAmountOfSectors() {
 		return TotalSectors;
 	}
-
 
 };
 
