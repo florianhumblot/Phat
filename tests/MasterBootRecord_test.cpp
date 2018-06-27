8de69704a4aff45cbfbd6d77d01a824bb7d0f232
Florian Humblot <florian.humblot@student.hu.nl>
2018-06-27 15:47:47 +0200

    Tests for the master boot record

---
+++ tests/MasterBootRecord_test.cpp
@@ -0,0 +1,27 @@
+#ifndef MBR_TEST
+#define MBR_TEST
+#include "test.hpp"
+#include <iostream>
+#include "../phat/Partition.hpp"
+#include "../phat/MasterBootRecord.hpp"
+#include "hwlib.hpp"
+
+class MBR_test : public MasterBootRecord {
+public:
+	MBR_test( Partition partitions[4] ) : MasterBootRecord( partitions[0], partitions[1], partitions[2], partitions[3] ) {}
+	void testMBR(TestSuite & TestSuite) {
+		TestSuite.assertEquals( isUsableDrive(), true, "MBR_test - isUsableDrive()" );
+		TestSuite.assertEquals( getFatAddress( 0 ), ( uint32_t ) 8192, "MBR_test - FatAddress for 0 partition" );
+		TestSuite.assertEquals( getFatAddress( 1 ), ( uint32_t ) 0, "MBR_test - FatAddress for 0 partition" );
+		TestSuite.assertEquals( getFatAddress( 2 ), ( uint32_t ) 0, "MBR_test - FatAddress for 0 partition" );
+		TestSuite.assertEquals( getFatAddress( 3 ), ( uint32_t ) 0, "MBR_test - FatAddress for 0 partition" );
+	}
+};
+class Empty_MBR_test : public MasterBootRecord {
+public:
+	Empty_MBR_test() : MasterBootRecord() {}
+	void testMBR( TestSuite & TestSuite ) {
+		TestSuite.assertEquals( isUsableDrive(), false, "Empty_MBR_test - isUsableDrive()" );
+	}
+};
+#endif
--- tests/main.cpp
+++ tests/main.cpp
@@ -3,16 +3,21 @@
 #include "BootSector_test.cpp"
 #include "DirectoryEntry_test.cpp"
 #include "Partition_test.cpp"
+#include "MasterBootRecord_test.cpp"
 #include <iostream>
 #include <bitset>
-#include "main.h"
+
 int main() {
 	TestSuite test = TestSuite();
+
+	/// start boot sector test
 	uint8_t data_[16] = { 0x02, 0x00, 0x08, 0x05, 0x18, 0x02, 0x00, 0x00, 0x1D, 0x74, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00 };
 	BPB_test bootsectorTest = BPB_test( data_ );
 	bootsectorTest.test_constructor( test );
 	bootsectorTest.testClusterToSector( test );
+	/// end boot sector test
 
+	/// start directory entry test
 	uint8_t data_short_entry[32] = { 0x54, 0x45, 0x53, 0x54, 0x20, 0x20, 0x20, 0x20, 0x54, 0x58, 0x54, 0x20, 0x18, 0x46, 0x86, 0x55,
 		0xD9, 0x4C, 0xD9, 0x4C, 0x00, 0x00, 0x8B, 0x55, 0xD9, 0x4C, 0x08, 0x00, 0x24, 0x00, 0x00, 0x00 };
 
@@ -25,15 +30,27 @@ int main() {
 
 	sfn.testDirectoryEntry( test );
 	lfnDir.testLFNDirectoryEntry( test );
+	/// end directory entry test
 
+	/// start partition test
 	uint8_t partition_data[16] = { 0x00, 0x82, 0x03, 0x00, 0x0C, 0xFE, 0x7F, 0xE1, 0x00, 0x20, 0x00, 0x00, 0x00, 0x0C, 0x76, 0x00 };
 	Partition_test partition = Partition_test( partition_data );
 
 	partition.testPartition( test );
+	/// end partition test
 
+	/// start mbr test
+	uint8_t empty_partition_data[16] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 };
+	Partition partitions[4] = { partition, Partition( empty_partition_data ), Partition( empty_partition_data ) , Partition( empty_partition_data ) };
+	MBR_test mbr = MBR_test( partitions );
+	Empty_MBR_test emptyMBR = Empty_MBR_test();
+	mbr.testMBR(test);
+	emptyMBR.testMBR(test);
+
+	/// test results
 	std::cout << "Amount of tests: " << +test.getTestCount() << std::endl;
 	std::cout << "Tests Passed: " << +test.getPassedTests() << std::endl;
-	std::cout << "Failed tests: " << std::endl;
+	std::cout << "Failed tests(" << test.test.size() << ") :" << std::endl;
 	for ( unsigned int i = 0; i < test.test.size(); i++ ) {
 		std::cout << "lhs = " << test.test[i][0] << " rhs = " << test.test[i][1];
 		std::cout << " lhs = " << std::bitset<32>( test.test[i][0] ) << " rhs = " << std::bitset<32>( test.test[i][1] ) << " Bitset equal: " << ( std::bitset<32>( test.test[i][0] ) == std::bitset<32>( test.test[i][1] ) ) << std::endl;
