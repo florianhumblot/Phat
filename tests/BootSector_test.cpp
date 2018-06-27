55e1463df4676aa78d867b11948a9166f22746b4
Florian Humblot <florian.humblot@student.hu.nl>
2018-06-27 14:58:54 +0200

    Added a string to figure out what test went wrong

--- tests/BootSector_test.cpp
+++ tests/BootSector_test.cpp
@@ -8,22 +8,22 @@ class BPB_test : public BootSector {
 public:
 	BPB_test( uint8_t data[16] ) : BootSector( data, address ) {}
 	void test_constructor( TestSuite & TestSuite ) {
-		TestSuite.assertEquals( BytesPerSector, ( uint16_t ) 512 );
-		TestSuite.assertEquals( SectorsPerCluster, ( uint8_t ) 8 );
-		TestSuite.assertEquals( ReserverdSectorCount, ( uint16_t ) 1304 );
-		TestSuite.assertEquals( NumberOfFats, ( uint8_t ) 2 );
-		TestSuite.assertEquals( SectorsPerFat, ( uint32_t ) 7540 );
-		TestSuite.assertEquals( RootCluster, ( uint32_t ) 2 );
-		TestSuite.assertEquals( RootEntryCount, ( uint16_t ) 0 );
-		TestSuite.assertEquals( FirstDataSector, ( uint32_t ) 24576 );
-		TestSuite.assertEquals( FatTablesAddress, ( uint32_t ) 9496 );
-		TestSuite.assertEquals( PartitionStart, ( uint32_t ) 8192 );
-		TestSuite.assertEquals( RootDirSectors, ( uint32_t ) 0 );
+		TestSuite.assertEquals( BytesPerSector, ( uint16_t ) 512, "BytesPerSector" );
+		TestSuite.assertEquals( SectorsPerCluster, ( uint8_t ) 8, "SectorsPerCluster" );
+		TestSuite.assertEquals( ReserverdSectorCount, ( uint16_t ) 1304, "ReserverdSectorCount" );
+		TestSuite.assertEquals( NumberOfFats, ( uint8_t ) 2, "NumberOfFats" );
+		TestSuite.assertEquals( SectorsPerFat, ( uint32_t ) 7540, "SectorsPerFat" );
+		TestSuite.assertEquals( RootCluster, ( uint32_t ) 2, "RootCluster" );
+		TestSuite.assertEquals( RootEntryCount, ( uint16_t ) 0, "RootEntryCount" );
+		TestSuite.assertEquals( FirstDataSector, ( uint32_t ) 24576, "FirstDataSector" );
+		TestSuite.assertEquals( FatTablesAddress, ( uint32_t ) 9496, "FatTablesAddress" );
+		TestSuite.assertEquals( PartitionStart, ( uint32_t ) 8192, "PartitionStart" );
+		TestSuite.assertEquals( RootDirSectors, ( uint32_t ) 0, "RootDirSectors" );
 	}
 
 	void testClusterToSector( TestSuite & TestSuite ) {
-		TestSuite.assertEquals( GetFirstSectorForCluster( 0 ), ( uint32_t ) 24560 );
-		TestSuite.assertEquals( GetFirstSectorForCluster( 6 ), ( uint32_t ) 24808 );
+		TestSuite.assertEquals( GetFirstSectorForCluster( 0 ), ( uint32_t ) 24560, "GetFirstSectorForCluster(0)" );
+		TestSuite.assertEquals( GetFirstSectorForCluster( 6 ), ( uint32_t ) 24608, "GetFirstSectorForCluster(6)" );
 	}
 };
 
--- tests/test.hpp
+++ tests/test.hpp
@@ -3,22 +3,25 @@
 #include "hwlib.hpp"
 #include <vector>
 #include <iostream>
-
+#include <string>
 class TestSuite {
 private:
 	int testCount = 0;
 	int passedTests = 0;
-
+	int i = 0;
 public:
 	std::vector<std::vector<uint32_t>> test;
 	TestSuite() {}
+
 	template <typename T >
-	bool assertEquals( T lhs, T rhs ) {
+	bool assertEquals( T lhs, T rhs, std::string name = "unknown" ) {
+		i++;
 		testCount++;
 		if ( lhs == rhs ) {
 			passedTests++;
 		} else {
 			std::vector<uint32_t> temp = { lhs, rhs };
+			std::cout << name << " failed" << std::endl;
 			test.emplace_back( temp );
 		}
 		return ( lhs == rhs );
